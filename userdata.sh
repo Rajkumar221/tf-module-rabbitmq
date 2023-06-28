9b06a283127ff6bcd8e1737a91f9acdbbc4f0a13
Rajkumar <rajkumarpatel52247@gmail.com>
2023-06-28 11:08:23 +0530

    s

---
+++ userdata.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+set-hostname -skip-apply ${component}
+labauto ansible
+ansible-pull -i localhost, -U https://github.com/rajkumar221/roboshop-ansible main.yml -e role_name=rabbitmq -e env=${env} &>>/opt/ansible.log
---
+++ variables.tf
@@ -0,0 +1,11 @@
+variable "component" {}
+variable "instance_type" {}
+variable "env" {}
+variable "sg_subnet_cidr" {}
+variable "tags" {}
+variable "vpc_id" {}
+variable "subnet_id" {}
+variable "allow_ssh_cidr" {}
+variable "zone_id" {}
+variable "kms_key_id" {}
+
